4bedcb37d2512805817ae3e98bfd4608516914ba
Francisca Caro <francisca@caro.cl>
2022-05-25 12:39:04 -0400

    home.js

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,71 +1,75 @@
-import {auth, provider, createUserWithEmailAndPassword, signInWithEmailAndPassword,onAuthStateChanged,signInWithRedirect,getRedirectResult,signOut} from "./init.js";
+import {
+  auth,
+  provider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithRedirect,
+  getRedirectResult,
+  signOut,
+} from './init.js';
 
 // State of user
 onAuthStateChanged(auth, (user) => {
-    if (user) {
-      console.log('user logged in:', user);
-    } else {
-      console.log('user logged out');
-    }
+  if (user) {
+    console.log('user logged in:', user);
+  } else {
+    console.log('user logged out');
+  }
 });
 
 // Create user with email and password
-const create = async (email, password)=>{
-    try {
-        await createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
-            console.log(userCredential);
-          })
-    } catch(error) {
-        throw error.message
-    }
-}
-
+const create = async (email, password) => {
+  try {
+    await createUserWithEmailAndPassword(auth, email, password).then(
+      (userCredential) => {
+        console.log(userCredential);
+      },
+    );
+  } catch (error) {
+    throw error.message;
+  }
+};
 
 // Sign in with email and password, la persona ya existe
-const login = async(email, password)=> {
-    try {
-        await signInWithEmailAndPassword(auth, email, password)
-        
-    } catch(error) {
-        throw error.message
-    }
+const login = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    throw error.message;
+  }
+};
+
+function google() {
+  signInWithRedirect(auth, provider);
+  getRedirectResult(auth)
+    .then((result) => {
+      // This gives you a Google Access Token. We can use tu access Google APIs
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+
+      // The signed-in user info
+      const user = result.user;
+    })
+    .catch((error) => {
+      // Handle Errors here
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the users account used
+      const email = error.customData.email;
+      // The AuthCredential type that was used
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
 }
 
+// Sign out, la persona existe
+signOut(auth).then(() => {
+  console.log('salió');
+});
 
-  function google (){
-    signInWithRedirect(auth, provider);
-    getRedirectResult(auth)
-      .then((result) => {
-        // This gives you a Google Access Token. We can use tu access Google APIs
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-  
-        // The signed-in user info
-        const user = result.user;
-      })
-      .catch((error) => {
-        // Handle Errors here
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the users account used
-        const email = error.customData.email;
-        // The AuthCredential type that was used
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
-      });
-  }
-    
-
-  // Sign out, la persona existe
-    signOut(auth).then(() => {console.log("salió")});
-  
-    
-
-
-
-
-export {login,google,create,signOut}
+export {
+  login, google, create, signOut,
+};
 
 //  signInWithEmailAndPassword(auth, email, password).then(() => {});
-
-
--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-import { login, google, create, signOut } from "./firebase/auth.js";
-import {auth} from "./firebase/init.js"
-import {routes,navigate} from "./router/router.js"
+import {
+  login, google, create, signOut,
+} from './firebase/auth.js';
+import { auth } from './firebase/init.js';
+import { routes, navigate } from './router/router.js';
 
-//VISTA LOGIN
+// VISTA LOGIN
 
-document.getElementById("root").innerHTML=routes[window.location.pathname];
+document.getElementById('root').innerHTML = routes[window.location.pathname];
 console.log(window.location);
 console.log(window.history);
 
-//sign up REGISTRAR
-const form1 = document.getElementById("form1");
+// sign up REGISTRAR
+const form1 = document.getElementById('form1');
 form1.addEventListener('submit', (e) => {
   e.preventDefault();
   const email = document.getElementById('email1').value;
@@ -20,20 +22,18 @@ form1.addEventListener('submit', (e) => {
 
 // Signing up with redirect Google
 const googleBtn = document.querySelector('#google-btn');
-googleBtn.addEventListener('click', function () {
- google();
+googleBtn.addEventListener('click', () => {
+  google();
 });
 
-//Log in ENTRAR
+// Log in ENTRAR
 const form = document.getElementById('form');
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    login(email, password);
-    form.reset();
-    navigate('/home');
-    console.log(window.history.length,"estoy en home");
-  });
-
-
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  login(email, password);
+  form.reset();
+  navigate('/home');
+  console.log(window.history.length, 'estoy en home');
+});
