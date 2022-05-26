4cd91e8a4e677a866a30ec70b51b5283f0c338da
soniarez <101676781+soniarez@users.noreply.github.com>
2022-05-26 10:14:58 -0400

    registerview

--- src/views/login.js
+++ src/views/login.js
@@ -1,7 +1,8 @@
-import {login, google, create} from '../firebase/auth.js';
-import { navigate } from "../router/router";
+/* eslint-disable import/no-cycle */
+import { login, google, create } from '../firebase/auth.js';
+import { navigate } from '../router/router';
 
-const Login =()=>{
+const Login = () => {
   const template = `
     <div id="login-box" class="login-box">
     <img src="#" id="logo" class="logo">
@@ -22,44 +23,16 @@ const Login =()=>{
         <br>
       </div>
     </form>
-    <form id="form1">
-      <div>
-        <label for="name">Name: </label>
-        <input id="name" type="name" placeholder="Your name">
-        <br>
-        <label for="email">Email: </label>
-        <input id="email1" type="email" placeholder="Your email">
-        <br>
-        <label for="password">Password: </label>
-        <input id="password1" type="password" placeholder="Your password">
-        <br>
-        <input id="sign-up" type="submit" value="Registrar"><!-- hacer popup: name, validación password -->
-        <br>
-        <button id="google-btn" class="google-btn">Sign Up with Google</button>
-      </div>
-    </form>
+    <div id="signup-btns" class="signup-btns">
+      <button id="sign-up" class="sign-up">Registrar</button>
+      <br>
+      <button id="google-btn" class="google-btn">Sign Up with Google</button>
+    </div>
     </div>`;
+
   const container = document.createElement('div');
   container.innerHTML = template;
   container.classList.add = 'container';
-
-  // sign up REGISTRAR
-  const form1 = container.querySelector('#form1');
-  form1.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const email = container.querySelector('#email1').value;
-    const password = container.querySelector('#password1').value;
-    create(email, password);
-    form1.reset();
-    navigate('/home');
-  });
-
-  // Signing up with redirect Google
-  const googleBtn = container.querySelector('#google-btn');
-  googleBtn.addEventListener('click', () => {
-    google();
-    navigate('/home');
-  });
 
   // Log in ENTRAR
   const form = container.querySelector('#form');
@@ -71,8 +44,21 @@ const Login =()=>{
     form.reset();
     navigate('/home');
   });
- 
+
+  // Sign Up REGISTRAR button, to register view
+  const signUpBtn = container.querySelector('#sign-up');
+  signUpBtn.addEventListener('click', () => {
+    navigate('/register');
+  });
+
+  // Signing up with redirect Google
+  const googleBtn = container.querySelector('#google-btn');
+  googleBtn.addEventListener('click', () => {
+    google();
+    navigate('/home');
+  });
+
   return container;
 };
-  
-export default Login ;
+
+export default Login;
---
+++ src/views/register.js
@@ -0,0 +1,42 @@
+/* eslint-disable import/no-cycle */
+import { create } from '../firebase/auth.js';
+import { navigate } from '../router/router.js';
+
+const Register = () => {
+  const template = `
+  <div id="login-box" class="login-box">
+    <img src="#" id="logo" class="logo">
+    <h2>Create your account!!</h2>
+    <form id="form1">
+    <div>
+      <label for="name">Name: </label>
+      <input id="name" type="name" placeholder="Your name">
+      <br>
+      <label for="email">Email: </label>
+      <input id="email1" type="email" placeholder="Your email">
+      <br>
+      <label for="password">Password: </label>
+      <input id="password1" type="password" placeholder="Your password">
+    </div>
+    <input id="create-account" type="submit" value="Crear Cuenta">
+    </div>
+  </form>
+  </div>`;
+
+  const container = document.createElement('div');
+  container.innerHTML = template;
+
+  // sign up REGISTRAR
+  const form1 = container.querySelector('#form1');
+  form1.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = container.querySelector('#email1').value;
+    const password = container.querySelector('#password1').value;
+    create(email, password);
+    form1.reset();
+    navigate('/home');
+  });
+  return container;
+};
+
+export default Register;
