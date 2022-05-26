7dbd394c19c94af98366e9f6be9fe91739d4aac5
Carolina <carolinazapatavidal@gmail.com>
2022-05-26 10:43:36 -0600

    first routes attempt

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+// import { myFunction } from './lib/index.js';
 
-myFunction();
+// myFunction();
+
+console.log(window.location.pathname);
+
+const home = '<h1>Home</h1>'
+
+const contact = '<h1>Contact</h1>'
+
+const routes = {'/home' : home, '/contact' : contact}
+
+const root = document.getElementById('root');
+
+root.innerHTML = routes[window.location.pathname];
