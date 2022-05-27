cc40e4b43b1b67146965f0e3989111d652fae686
soniarez <101676781+soniarez@users.noreply.github.com>
2022-05-27 17:52:14 -0400

    refactore de funciones auth

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import {
   auth,
   provider,
@@ -9,6 +10,19 @@ import {
   signOut,
   sendEmailVerification,
 } from './init.js';
+
+// INTERPRETANDO LOS MÉTODO DE FIREBASE;
+/* createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {           comentario:  // El then() es la promesa que nos entrega Firebase
+    // Signed in
+    const user = userCredential.user;   comentario: // Esto es lo que entrega la promesa, su resultado, es lo que espera con el await
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  }); */
 
 // State of user
 onAuthStateChanged(auth, (user) => {
@@ -22,63 +36,52 @@ onAuthStateChanged(auth, (user) => {
 // Create user with email and password
 const create = async (email, password) => {
   try {
-    await createUserWithEmailAndPassword(auth, email, password).then(
-      () => {
-        sendEmailVerification(auth.currentUser);
-      },
-    );
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password); // La Promesa que recibimos se vuelve el callback al método del Firebase
+    await sendEmailVerification(auth.currentUser);
+    return userCredential.user.uid; // retornamos el resultado de la Promesa, según lo que vayamos a usar para nuestro programa
   } catch (error) {
-    throw error.message;
+    if (email === '') {
+      console.log('debes poner un email');
+    } if (password.length <= 5) {
+      console.log('debes ingresar contraseña de al menos 6 caracteres');
+    }
+    return null;
   }
 };
-
-// email validation
-sendEmailVerification(auth.currentUser)
-  .then(() => {
-    console.log('revisa tu inbox');
-    // ...
-  }); 
 
 // Sign in with email and password, la persona ya existe
 const login = async (email, password) => {
   try {
-    await signInWithEmailAndPassword(auth, email, password);
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    return userCredential.user.uid;
   } catch (error) {
-    throw error.message;
+    console.log('error login', error);
+    return null;
   }
 };
 
-function google() {
+// Sign in with Google
+const google = async () => {
   signInWithRedirect(auth, provider);
-  getRedirectResult(auth)
-    .then((result) => {
-      // This gives you a Google Access Token. We can use tu access Google APIs
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-
-      // The signed-in user info
-      const user = result.user;
-    })
-    .catch((error) => {
-      // Handle Errors here
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the users account used
-      const email = error.customData.email;
-      // The AuthCredential type that was used
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
-    });
-}
+  try {
+    const userCredential = await getRedirectResult(auth);
+    return userCredential.user.uid;
+  } catch (error) {
+    console.log('error signin with Google', error);
+    return null;
+  }
+};
 
 // Sign out, la persona existe
-const out = () => {
-  signOut(auth).then(() => {
+const out = async () => {
+  try {
+    await signOut(auth);
     console.log('salió');
-  });
+  } catch (error) {
+    console.log('error signout', error);
+  }
 };
 
 export {
   login, google, create, out,
 };
-
