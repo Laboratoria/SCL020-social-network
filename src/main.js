59d4008bf966e2d59f07dbac4bb70bcd8a0a78b4
genesis <gnsslr@gmail.com>
2022-05-31 18:45:50 -0400

    creando primera vista

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
-// Este es el punto de entrada de tu aplicacion
+let container= document.getElementById("container");
 
-import { myFunction } from './lib/index.js';
+let firstView= document.createElement("div");
+firstView.setAttribute("class", `firstView`);
+container.appendChild(firstView);
 
-myFunction();
+let logo= document.createElement("div");
+logo.setAttribute("class", `logo`);
+let logoImage= document.createElement("img");
+logoImage.setAttribute("src", "./Social-Images/logoescuela.webp");
+logo.appendChild(logoImage);
+firstView.appendChild(logo);
+
+let currentUserData = document.createElement("div");
+currentUserData.setAttribute("class", `currentUserData`);
+firstView.appendChild(currentUserData);
+let userEmail= document.createElement("input");
+userEmail.setAttribute("inputText", 'Introduzca su correo electrónico');
+//document.addEventListener("DOMContentLoaded", () => { prueba(logo);});
