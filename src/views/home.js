14f85004a9dd7393bd8916c89d45ff9831c01624
soniarez <101676781+soniarez@users.noreply.github.com>
2022-06-01 10:26:25 -0400

    cambio en botón

--- src/views/home.js
+++ src/views/home.js
@@ -1,13 +1,13 @@
-/* eslint-disable import/no-cycle */
-import { navigate } from '../router/router.js';
-import { out } from '../firebase/auth.js';
+//import { navigate } from '../router/router.js';
+//import { out } from '../firebase/auth.js';
 import { Header } from '../utils/header.js';
 import { Footer } from '../utils/footer.js';
 
 const Home = () => {
   const template = `
   
-  <button id="sign-out" class="main-btn">Sign Out</button>`;
+  <!--<button id="sign-out" class="main-btn">Sign Out</button>-->
+  <h1>Por ahora soy vista Home</h3>`;
 
   const container = document.createElement('div');
   container.className = 'home-page';
@@ -16,11 +16,11 @@ const Home = () => {
 
   container.append(Header(), Footer());
 
-  const buttonOut = container.querySelector('#sign-out');
+/*   const buttonOut = container.querySelector('#sign-out');
   buttonOut.addEventListener('click', () => {
     out();
     navigate('/');
-  });
+  }); */
 
   return container;
 };
