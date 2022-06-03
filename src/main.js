75de2d6ae89ad9ea831e98152189054f011e7e33
Sol <solemich@gmail.com>
2022-06-03 09:48:18 -0400

    primer sprint

--- src/main.js
+++ src/main.js
@@ -1,5 +1,42 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+// import { myFunction } from './lib/index.js';
 
-myFunction();
+// myFunction();
+
+ //appendchild inyecta en el html el elemento que se creó en el JS (son hijos de body <-- se puede cmabiar dps)
+ //onpopstate es el que copia la URL y lo mete en el history list (historial de navegacion) permitiendo ir a la pagina anterior o adelante segun corresponda
+
+
+let a= document.createElement("button");
+a.innerText ="Home"
+let b= document.createElement("button");
+b.innerText ="Registro"
+let c= document.createElement("button");
+c.innerText ="Eventos"
+// Firefox guarda los objetos estado en el disco del usuario
+//objeto estado=primer parametro del pushstate
+a.addEventListener('click', (e)=>{
+  history.pushState({page:'page'}, '','Home')
+})
+document.body.appendChild(a)
+
+  b.addEventListener('click', (e1)=>{
+  history.pushState({page1:'page1'}, '','Registro')
+})
+document.body.appendChild(b)
+
+c.addEventListener('click', (e2)=>{
+  history.pushState({page2:'page2'}, '','Eventos')
+})
+document.body.appendChild(c)
+
+window.onpopstate= (e)=>{
+  e.state.page
+}
+window.onpopstate= (e1)=>{
+  e1.state.page1
+}
+window.onpopstate= (e2)=>{
+  e2.state.page2
+}
---
+++ src/router/router.js
@@ -0,0 +1,46 @@
+import { navGuard} from './firebase/auth.js'
+
+import Home from '../views/home.js'
+
+import Login from '../views/login.js'
+
+const routes = [
+    { path:'/',
+      name:'Home',
+      template: Home()
+
+      },  
+
+    { path:'login',
+      name:'Login',
+      template: Login()
+
+    }
+]    
+    
+const renderTemplate = () => {
+    const view = routes.filter(route => route.path === window.location.pathname)[0]
+    const container = document.getElementById('app')
+    container.innerHTML = ''
+    container.appendChild(view.template)
+
+}
+
+const next = (path) => {
+    const view = routes.filter(route => route.path === path)[0] 
+    history.pushState({}, view.name, view.path) 
+    renderTemplate()
+
+}
+
+const navigate = (path) => { navGuard (next, path, '/login') }
+
+
+const initRouter = () => {
+    navGuard(next,'/', 'login' )
+    renderTemplate()
+    window.onpopstate = ()=> {renderTemplate()}
+
+
+}
+
