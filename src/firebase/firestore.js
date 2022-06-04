aadcf26dc957456c0ca3a4f660e9189173315854
Trinidad Medina <trinidad.medina.silva@gmail.com>
2022-06-04 19:33:20 -0400

    post-template

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -1,50 +1,38 @@
 import {
-  db, collection, addDoc, getDocs,
+  db, collection, addDoc, getDocs, auth
 } from './init.js';
 
-// Creating Users collection and adding new docs to collection
-const createDoc = async (userId, userName) => {
-  try {
-    const docRef = await addDoc(collection(db, 'users'), {
-      user: userId,
-      name: userName,
-    });
-    console.log('Document written with ID: ', docRef.id);
-    return docRef;
-  } catch (error) {
-    console.log('esta wea no funciona');
-    return null;
-  }
-};
 
 // Creating Posts collection and adding new docs to collection
-const createPost = async (textPost) => {
-  try {
-    const postRef = await addDoc(collection(db, 'posts'), {
-      description: textPost,
-    });
-    return postRef;
-  } catch (error) {
-    console.log('post no funciona');
-    return null;
-  }
+const createPost = async (review,movie,country) => {
+  let userName;
+    if (auth.currentUser.displayName === null) {
+        userName = auth.currentUser.email;
+      } else {
+        userName = auth.currentUser.displayName;
+      };
+    addDoc(collection(db, 'posts'), {
+      userName,
+      userId:auth.currentUser.uid,
+      review,
+      movie,
+      country,
+      date:Date(Date.now()), //cambiar por TIMESTAMP
+    })
 };
+
 
 // Reading Post
 const readingPost = async () => {
   const results = await getDocs(collection(db, 'posts'));
-  const list = document.createElement('ul');
+  const listItem = [];
+/*   results.forEach((doc) => {
+    listItem.push(doc.data())
+  }); */
 
-  results.forEach((doc) => {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = doc.data().description;
-
-    list.appendChild(listItem);
-  });
-
-  return list;
+  return results;
 };
 
 
-export { createDoc, createPost, readingPost };
+export { createPost, readingPost };
 
--- src/views/home.js
+++ src/views/home.js
@@ -14,8 +14,38 @@ const Home = () => {
 
 // Printing posts, header and footer to home page
 const refetch = async () => {
+  container.innerHTML="";
+  let postStructure = '';
   const list = await readingPost();
-  container.append(Header(), list, Footer());
+  list.forEach((doc)=>{
+    const posts = doc.data();
+    postStructure += `
+    <div class="post-border">
+      <div class='post'>
+        <span class="date">${posts.date}</span><br>
+        <p class="user-container">   
+          <i class="user-name">${posts.userName}</i> 
+        dijo: </p>
+        <h3>${posts.movie}</h3>
+        <p>${posts.review}</p>
+        <div>
+          <div class='info-container'>
+            <p class='info'><b>País:</b> ${posts.country} </p>
+          </div>
+          <button class='btn-delete btn' data-id='${doc.id}'>Delete</button>
+          <button class='btn-edit btn' data-id='${doc.id}'>Edit</button>       
+        </div>
+      </div>
+      <div class="likes-border"> 
+        <button  class="btn-like" value=${doc.id}>
+          <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="2em" height="2em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10s10-4.486 10-10S17.514 2 12 2zm4.186 10.74L12 16.926L7.814 12.74a2.745 2.745 0 0 1 0-3.907a2.745 2.745 0 0 1 3.906 0l.28.279l.279-.279a2.745 2.745 0 0 1 3.906 0a2.745 2.745 0 0 1 .001 3.907z"/></svg> 
+        </button>
+      </div>
+    </div>
+    `
+ })
+ container.innerHTML = postStructure;
+  container.append(Header(), Footer());
 };
 refetch();
 
