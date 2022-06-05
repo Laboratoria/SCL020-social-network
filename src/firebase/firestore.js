121607dd7ec81f8f0db28495b642d7b829151f0b
soniarez <101676781+soniarez@users.noreply.github.com>
2022-06-05 01:45:04 -0400

    cambios en vista home posts

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -1,5 +1,5 @@
 import {
-  db, collection, addDoc, getDocs, auth, serverTimestamp, query, orderBy, limit
+  db, collection, addDoc, getDocs, auth, serverTimestamp, query, orderBy, limit,
 } from './init.js';
 
 // Creating Posts collection and adding new docs to collection
@@ -23,19 +23,20 @@ const createPost = async (review, movie, country) => {
 // Reading Post
 const readingPost = async () => {
   const result = await getDocs(collection(db, 'posts'));
-  const q = query(result.query, orderBy('date', 'desc'));
-  //console.log(q);
+  const q = query(result.query, orderBy('date', 'desc'), limit(5));
+  // console.log(q);
   const filterQ = await getDocs(q);
-  //console.log(filterQ);
+  // console.log(filterQ);
   return filterQ.docs;
 };
 
+// Country View Posts
 const countryPosts = async (country) => {
-  const q = query(collection(db, "posts"), where("country", "==", country), orderBy('date','desc'));
+  const q = query(collection(db, 'posts'), where('country', '==', country), orderBy('date', 'desc'));
   const querySnapshot = await getDocs(q);
   return filterQ.docs;
-}
+};
 
 export { createPost, readingPost, countryPosts };
 
-//const q = query(result.query, orderBy('date', 'desc'), limit(5)); // , limit(n)
+// const q = query(result.query, orderBy('date', 'desc'), limit(5)); // , limit(n)
