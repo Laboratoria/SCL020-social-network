236ed1c9da2ef587721b8f0a28bf94339a84ff8f
genesis <gnsslr@gmail.com>
2022-06-06 12:24:08 -0400

    conexión de firebase

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 //import { myFunction } from './lib/index.js';
 import { home } from './lib/Views/templateHome.js';
 import { changeRoute } from './lib/router.js';
-
+import {app} from './firebase-doc/firebase.js';
+console.log(app);
 // myFunction();
 window.addEventListener('load', () =>{
     window.history.replaceState({}, '', window.location.origin+'#/home' )
