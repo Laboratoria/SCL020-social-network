d362e3437390e3554142071a0b4fbdbe77dc4919
Constanza <c.leytonleiva@gmail.com>
2022-06-07 17:58:05 -0400

    template error 404

---
+++ src/lib/view/template404.js
@@ -0,0 +1,12 @@
+export const error404 = () => {
+    const divError404 = document.createElement('div')
+    divError404.setAttribute('id', 'error404View')
+    const viewError404 = `
+        <div id="errorContent">
+            <img src="img/404.png" alt="Error 404">
+            <a href="#/home"> <button class="button"></button></a>
+        </div>
+        `
+        divError404.innerHTML= viewError404;
+        return divError404
+};
