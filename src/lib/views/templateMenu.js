669b71178d4bb041adea6700b08da89816492137
fabibbc <fbelenbc@gmail.com>
2022-06-09 11:04:14 -0400

    'adding img back'

--- src/lib/views/templateMenu.js
+++ src/lib/views/templateMenu.js
@@ -6,9 +6,9 @@ export const menu = () =>{
         <div class="sub-container-menu">
             <a href="#/feed"><button class="btn-backFeed"> <- </button></a>
             <ul class="container-ul">
-                <label class="container-img"><img class="img-menu" src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-profile.png?raw=true" alt="logo-profile"><li><a href="#/profile">Profile</a></li></label>
-                <label class="container-img"><img class="img-menu" src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/Feed.png?raw=true" alt="logo-feed"><li><a href="#/feed">Feed</a></li></label>
-                <label class="container-img"><img class="img-menu" src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/sing%20out.png?raw=true" alt="logo-LogOut"><li><a href="#/logoutConfirmation">Log Out</a></li></label>
+                <label class="container-img"><img class="img-menu" src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-profile%20(1).png?raw=true" alt="logo-profile"><li><a href="#/profile">Profile</a></li></label>
+                <label class="container-img"><img class="img-menu" src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-feed.png?raw=true" alt="logo-feed"><li><a href="#/feed">Feed</a></li></label>
+                <label class="container-img"><img class="img-menu" src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-signout%20(1).png?raw=true" alt="logo-LogOut"><li><a href="#/logoutConfirmation">Log Out</a></li></label>
             </ul>
         </div>
     </div>
