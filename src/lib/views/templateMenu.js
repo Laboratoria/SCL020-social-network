297aa04782c3af8f6036173962617307b42a8ddb
fabibbc <fbelenbc@gmail.com>
2022-06-09 12:24:24 -0400

    adding img hamburger

--- src/lib/views/templateMenu.js
+++ src/lib/views/templateMenu.js
@@ -4,12 +4,21 @@ export const menu = () =>{
     `
     <div class="container-menu">
         <div class="sub-container-menu">
-            <a href="#/feed"><button class="btn-backFeed"> <- </button></a>
-            <ul class="container-ul">
-                <label class="container-img"><img class="img-menu" src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-profile%20(1).png?raw=true" alt="logo-profile"><li><a href="#/profile">Profile</a></li></label>
-                <label class="container-img"><img class="img-menu" src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-feed.png?raw=true" alt="logo-feed"><li><a href="#/feed">Feed</a></li></label>
-                <label class="container-img"><img class="img-menu" src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-signout%20(1).png?raw=true" alt="logo-LogOut"><li><a href="#/logoutConfirmation">Log Out</a></li></label>
-            </ul>
+            <a href="#/feed"><button class="btn-backFeed"><img src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/return.png?raw=true" alt="btn-backFeed"></button></a>
+            <div class="container-img">
+                <div class="sub-container-img">
+                    <img class="img-menu" src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-profile%20(1).png?raw=true" alt="logo-profile">
+                    <a href="#/profile">Profile</a>
+                </div>
+                <div class="sub-container-img">
+                    <img class="img-menu" src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-feed.png?raw=true" alt="logo-feed">
+                    <a href="#/profile">Feed</a>
+                </div>
+                <div class="sub-container-img">
+                    <img class="img-menu" src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-signout%20(1).png?raw=true" alt="logo-LogOut">
+                    <a href="#/profile">Log out</a>
+                </div>
+            </div>
         </div>
     </div>
     `
