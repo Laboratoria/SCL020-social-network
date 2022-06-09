6b14776decb80e6252be2588faca538ea4c42e3d
soniarez <101676781+soniarez@users.noreply.github.com>
2022-06-09 11:23:38 -0400

    saca firebase from gitignore

---
+++ src/firebase/configFirebase.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDQf-NYH9EHrDoXWGMJbufYgI4iaXr_j_0',
+  authDomain: 'junktube-d40c2.firebaseapp.com',
+  projectId: 'junktube-d40c2',
+  storageBucket: 'junktube-d40c2.appspot.com',
+  messagingSenderId: '993784150979',
+  appId: '1:993784150979:web:208ce71330c4274f495b76',
+};
