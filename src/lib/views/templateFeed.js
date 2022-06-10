6b4575b690068bd4d8475d566486511f66ed3098
fabibbc <fbelenbc@gmail.com>
2022-06-10 11:32:46 -0400

    'CSS and adding logo img'
    Co-authored-by: SvillarroelZ <SvillarroelZ@users.noreply.github.com>

--- src/lib/views/templateFeed.js
+++ src/lib/views/templateFeed.js
@@ -6,32 +6,32 @@ export const feed = () => {
         <main class="feed">
             <nav>
                 <a href="#/menu"><img class="menu-burger" src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/lista.png?raw=true" alt="logo-ticket"></a>
-                <input type="text" placeholder="U+1F50E Search">
+                <input type="text" placeholder="Search">
             </nav>
             <div class="movies">
-                <button>Movie1</button>
-                <button>Movie2</button>
-                <button>Movie3</button>
-                <button>Movie4</button>
+                <button><img class="movie-rank" src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-profile%20(1).png?raw=true" alt="movie-rank"></button>
+                <button><img class="movie-rank" src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-profile%20(1).png?raw=true" alt="movie-rank"></button>
+                <button><img class="movie-rank" src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-profile%20(1).png?raw=true" alt="movie-rank"></button>
+                <button><img class="movie-rank" src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-profile%20(1).png?raw=true" alt="movie-rank"></button>
             </div>
             <div>
                 <div class="container-post">
                     <div class="post">
                         <div class="user-info">
-                            <img src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-profile%20(1).png?raw=true" alt="user-img">
-                            <p>@user_name</p>
+                            <img src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-user-post1.png?raw=true" alt="user-img">
+                            <p>@User_name</p>
                         </div>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, modi nam!</p>
-                        <button class="btn-popCorn">PopCorn</button>
+                        <p class="post-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, modi nam!</p>
+                        <button class="btn-popCorn">🍿</button>
                         <!-- <button class="btn-comment">Comment</button> -->
                     </div>
                     <div class="post">
                         <div class="user-info">
-                            <img src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-profile%20(1).png?raw=true" alt="user-img">
+                            <img src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-user-post1.png?raw=true" alt="user-img">
                             <p>@user_name</p>
                         </div>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, modi nam!</p>
-                        <button class="btn-popCorn">PopCorn</button>
+                        <p class="post-p" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, modi nam!</p>
+                        <button class="btn-popCorn">🍿</button>
                         <!-- <button class="btn-comment">Comment</button> -->
                     </div>
                 </div>
