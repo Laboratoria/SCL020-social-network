5d553f04e85992b0248feb64c9c23cc0a9519e01
fabibbc <fbelenbc@gmail.com>
2022-06-10 16:59:15 -0400

    'fixing search css'

--- src/lib/views/templateFeed.js
+++ src/lib/views/templateFeed.js
@@ -3,14 +3,16 @@ export const feed = () => {
 
     const viewFeed = //html
     `
-        <main class="feed">
+        <main class="container-feed">
             <nav class="nav-burguer-search">
-                <a href="#/menu"><img class="menu-burger" src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/lista.png?raw=true" alt="logo-ticket"></a>
-                <div class="search-box">
-                    <input class="search-input"type="text" name="" placeholder="Search...">
-                    <button class="search-button" href="#">
-                        <i class="material-icons">search</i>
-                    </button>
+                <div class="menu-feed"><a href="#/menu"><img class="menu-burger" src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/lista.png?raw=true" alt="logo-ticket"></a></div>
+                <div class="search-bar">
+                    <div class="search-box">
+                        <input class="search-input"type="text" name="" placeholder="Search...">
+                        <button class="search-button" href="#">
+                            <i class="material-icons">s</i>
+                        </button>
+                    </div>
                 </div>
             </nav>
             <div class="movies">
@@ -18,27 +20,25 @@ export const feed = () => {
                 <button><img class="movie-rank" src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-profile%20(1).png?raw=true" alt="movie-rank"></button>
                 <button><img class="movie-rank" src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-profile%20(1).png?raw=true" alt="movie-rank"></button>
                 <button><img class="movie-rank" src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-profile%20(1).png?raw=true" alt="movie-rank"></button>
-            </div>
-            <div>
-                <div class="container-post">
-                    <div class="post">
-                        <div class="user-info">
-                            <img src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-user-post1.png?raw=true" alt="user-img">
-                            <p>@User_name</p>
-                        </div>
-                        <p class="post-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, modi nam!</p>
-                        <button class="btn-popCorn">🍿</button>
-                        <!-- <button class="btn-comment">Comment</button> -->
+            </div>   
+            <div class="container-post">
+                <div class="post">
+                    <div class="user-info">
+                        <img src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-user-post1.png?raw=true" alt="user-img">
+                        <p>@User_name</p>
                     </div>
-                    <div class="post">
-                        <div class="user-info">
-                            <img src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-user-post1.png?raw=true" alt="user-img">
-                            <p>@user_name</p>
-                        </div>
-                        <p class="post-p" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, modi nam!</p>
-                        <button class="btn-popCorn">🍿</button>
+                    <p class="post-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, modi nam!</p>
+                    <button class="btn-popCorn">🍿</button>
                         <!-- <button class="btn-comment">Comment</button> -->
+                </div>
+                <div class="post">
+                    <div class="user-info">
+                        <img src="https://github.com/fabibbc/SCL020-social-network/blob/main/src/img/logo-user-post1.png?raw=true" alt="user-img">
+                        <p>@user_name</p>
                     </div>
+                    <p class="post-p" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, modi nam!</p>
+                    <button class="btn-popCorn">🍿</button>
+                        <!-- <button class="btn-comment">Comment</button> -->
                 </div>
             </div>
         </main>
