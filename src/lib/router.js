b5b37b40fd9499adcc157a9597e445d28d58de3d
CarolinaColmenares <yzcarcolmenares@gmail.com>
2022-06-13 08:59:38 -0400

    CSS Search

--- src/lib/router.js
+++ src/lib/router.js
@@ -3,7 +3,7 @@ import { logIn } from './view/templateLogIn.js';
 import { home } from './view/templateHome.js';
 import { error404 } from './view/template404.js';
 import { userProfile } from './view/templateUserProfile.js';
-import { searchPost } from './view/templateSearch';
+import { searchPost } from './view/templateSearch.js';
 
 export const changeRoute = (hash) => {
     if (hash === '#/') {
