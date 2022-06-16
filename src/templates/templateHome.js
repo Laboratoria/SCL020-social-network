1d3a5367264fe7afe47ca5a1ba8eea7806e3295d
ClaudiaMorales22 <claudia.extreme2204@gmail.com>
2022-06-16 15:37:59 -0400

    función sendEmailVerification activa

--- src/templates/templateHome.js
+++ src/templates/templateHome.js
@@ -1,6 +1,6 @@
-import { signingInWithgoogle } from "../firebase-doc/authentication.js";
-import { getRedirectResult, auth } from '../firebase-doc/firebase.js'
-import { changeRoute } from "../lib/router.js";
+import { signingInWithgoogle } from '../firebase-doc/authentication.js';
+import { getRedirectResult, auth } from '../firebase-doc/firebase.js';
+import { changeRoute } from '../lib/router.js';
 
 export const home = () => {
   const viewHome = // html
@@ -17,28 +17,26 @@ export const home = () => {
     <p class="registerChoice"><strong>¿No tienes una cuenta?</strong></p>
     <a href="#/register" class="register"><strong>REGISTRATE</strong></a>
     </div>
-    `
-  ;
+    `;
+  const container = document.createElement('div');
+  container.innerHTML = viewHome;
+  container.className = 'firstViewContainer';
+  const googleButton = container.querySelector('.loginGoogle');
+  googleButton.addEventListener('click', async (e) => {
+    const user = await signingInWithgoogle();
+    console.log(user);
+    if (user) {
+      changeRoute('/#wall');
+    } else (console.log('error'));
+  });
+  const inWithGoogle = async () => {
+    const redirected = await getRedirectResult(auth);
+console.log(redirected);
+    if (redirected) { changeRoute('#/wall'); }
+  };
 
-const container = document.createElement('div');
-container.innerHTML= viewHome;
-container.className = 'firstViewContainer';
-const googleButton = container.querySelector('.loginGoogle');
-googleButton.addEventListener('click', async (e) => {
-  const user = await signingInWithgoogle();
-  console.log(user)
-  if (user) {
-    changeRoute('/#wall')
-  }
-  else (console.log('error'))
-})
-const inWithGoogle = async () =>  {const redirected = await getRedirectResult(auth)
- 
-if (redirected){changeRoute('#/wall') }};
-              
-             // console.log(inWithGoogle)
-             // return inWithGoogle.user.uid;
-            inWithGoogle()
+  // console.log(inWithGoogle)
+  // return inWithGoogle.user.uid;
+  inWithGoogle();
   return container;
 };
-
