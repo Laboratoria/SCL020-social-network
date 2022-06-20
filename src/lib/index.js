51fe488f662f44f42822ea129883c261f262e293
FernandaFloresC <fernanda.flores.c.99@gmail.com>
2022-06-20 11:56:06 -0400

    agregamos los templates y routes de prueba

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,8 @@
 // aqui exportaras las funciones que necesites
 
+//Templates de la pantalla
+
+//Funcion que crea el template de la pantalla Inicio
 export const templateIndex = () => {
   const screenIndex =  `<section>
     <section>
@@ -28,7 +31,57 @@ return screenIndex;
   
 };
 
-export const myFunction = () => {
-  // aqui tu codigo
-  console.log('Hola mundo!');
+//Funcion que crea el template de la pantalla Login
+export const templateLogin = () => {
+  const screenLogin = `<section>
+  <section>
+    <img src="" alt="">
+  </section>
+  <form action="">
+  <label for="Correo">
+    Correo/Alias: <input type="text">
+  </label>
+  <label for="Contraseña">
+    Contraseña: <input type="text">
+  </label>
+</form>
+  <section>
+  <button>
+    INICIAR SESIÓN
+  </button>
+  <p>
+    ¿No tienes cuenta aún? Regístrate <a href="">aquí.</a>  
+  </p>
+  </section>
+</section>
+`;
+  //console.log('Hola mundo!');
+};
+
+export const templateRegister = () => {
+  const screenRegister =  `<section>
+    <section>
+      <img src="" alt="">
+    </section>
+    <article>
+<form action="Registro de Sesión">
+  <label for="Name">Nombre y Apellido<input type="text"></label>
+  <label for="Date of birth">Fecha de nacimiento<input type="month" id="seleccionarMes" min="1940-00" max="2020-01" /></label>
+  <label for="Email">Email<input type="text"></label>
+  <label for="Password">Contraseña<input type="text"></label>
+  <label for="Confirmar contraseña">Confirmar contraseña<input type="text"></label> 
+  <label for="Alias">Alías<input type="text"></label> 
+</form>
+    </article>
+    <section>
+    <button>
+      Listo
+      (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
+    </button>
+   
+    </section>
+  </section>
+  `;
+return screenIndex;
+  
 };
---
+++ src/lib/routes.js
@@ -0,0 +1,17 @@
+
+
+
+const PATHS = {
+    home: {
+        path: "/",
+        template: `<h1>🏠 Home</h1>`,
+    },
+    Login: {
+        path: "/about",
+        template: `<h1>👩🏻‍💻 Sobre mi</h1>`,
+    },
+    Registro: {
+        path: "/contact",
+        template: `<h1>📱 Contacto</h1>`,
+    }
+}
