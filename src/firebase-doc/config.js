a5bd7e3dfc143cb69f56df2b30ebf4e6b2d2bba5
ClaudiaMorales22 <claudia.extreme2204@gmail.com>
2022-06-23 10:27:13 -0400

    corrección VE

--- src/firebase-doc/config.js
+++ src/firebase-doc/config.js
@@ -1,10 +1,8 @@
-import 'dotenv/config';
-
 export default {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-};
+  apiKey: "AIzaSyAlaxEjobH6AfdehaDFXaqU62SSNFPJJH8",
+  authDomain: "school-is-cool-95214.firebaseapp.com",
+  projectId: "school-is-cool-95214",
+  storageBucket: "school-is-cool-95214.appspot.com",
+  messagingSenderId: "374908144655",
+  appId: "1:374908144655:web:7ba0302ba69f6724d9277a"
+  };
