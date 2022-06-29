c7c03702b6a8eaf283b923c261938cc8bbe9e9d4
Angelica Contreras <afernandac94@gmail.com>
2022-06-29 15:48:47 -0400

    Ajustes CSS

--- src/lib/views/viewSignUp.js
+++ src/lib/views/viewSignUp.js
@@ -1,29 +1,29 @@
 //Este archivo es para registrarse
 export const interpSignUp = () => {
 	const divSignUp = document.createElement('div');
-	const formSignUp = //html
+	const formSignUp =
+		//html
 		`
       <div id="rootSingnUp" class="containerSignUp">
-        <div>
-          <a href="#/" id="back" class="buttonSingUp">Atras</a>
+        <div class="containerButtonBack">
+          <a href="#/" id="back" class="buttonBack">Atrás</a>
         </div>
         <form action="" class="containerFormSignUp">
-            <h1 class="REGISTRO">Registro de Usuario</h1>
+            <h1 class="tittleRegistroUser">Registro de Usuario</h1>
             <input type="email" id="email" placeholder="CORREO ELECTRÓNICO" autofocus class="campotxt" autocomplete="off" required>
             <input type="text" id="password" placeholder="CONTRASEÑA" autofocus class="campotxt" autocomplete="off" required minlength="6" maxlength="8">
-            <button id="next" class="NEXT">Siguiente</button>
+            <button id="next" class="next">Siguiente</button>
         </form>
       </div>
     `;
-const siguiente = document.querySelector("#next");
-const email = document.getElementById("email");
-const password = document.querySelector("#password");
-
+	const siguiente = document.querySelector('#next');
+	const email = document.getElementById('email');
+	const password = document.querySelector('#password');
 
 	divSignUp.innerHTML = formSignUp;
-  // siguiente.addEventListener("submit", (event) =>{
-  //   event.preventDefault();
-  //   console.log("prueba");
-  // })
+	// siguiente.addEventListener("submit", (event) =>{
+	//   event.preventDefault();
+	//   console.log("prueba");
+	// })
 	return divSignUp;
 };
