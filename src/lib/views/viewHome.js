31db7d6c5a4b6ac63143d8bf1bc5d688cd091bc2
camicamposc <clearca57@gmail.com>
2022-06-30 17:55:17 -0400

    Creacion de Footer y su estilo en CSS

--- src/lib/views/viewHome.js
+++ src/lib/views/viewHome.js
@@ -4,24 +4,30 @@ export const interpHome = () => {
 
 	const formHome = //html
   `
-    <div id="rootHome" class="containerHome">
-      <section class="textBox">
-        <div class="textInput">
-        <input type="text" id="postTittle" class="boxtxt" placeholder="Título">
-        <p></p>
-        <textarea type="text" id="postText" class="boxtxt" placeholder="Escribe tu publicación aquí..."></textarea>
+    <div id="rootHome" class="containerRootHome">
+      <section class="containerHome">
+        <div class="textBox">
+          <div class="textInput">
+          <input type="text" id="postTittle" class="boxtxt" placeholder="Título">
+          <p></p>
+          <textarea type="text" id="postText" class="boxtxt" placeholder="Escribe tu publicación aquí..."></textarea>
+          </div>
+          <a href=''>
+          <button id="Post" class="post">Publicar</button>
+          </a>
+        </div> 
+        <div class="textBox">
+          <input type="text" id="forPosting"class="boxtxt">
+          <a href='' id="Post">
+          <input type="image" src="images/heart.png" class="LIKE" />
+          </a>
         </div>
-        <a href=''>
-        <button id="Post" class="post">Publicar</button>
-        </a>
-      </section> 
-      <div class="textBox">
-        <input type="text" id="forPosting"class="boxtxt">
-        <a href='' id="Post">
-        <input type="image" src="images/heart.png" class="LIKE" />
-
-        </a>
-      </div>
+      </section>
+      <footer>
+        <input type="image" src="images/log-out.png" id="logOut" class="imageFooter">      
+        <input type="image" src="images/home.png" id="home" class="imageFooter">      
+        <input type="image" src="images/clima.png" id="topicChange" class="imageFooter">
+      </footer>
     </div>
   `;
 	divHome.innerHTML = formHome;
