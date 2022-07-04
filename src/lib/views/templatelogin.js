588675bd71d24bc1dfad127025526f4003cb8f96
ErickaAndreinaCortes <erickaandreinacortes@gmail.com>
2022-07-04 15:11:10 -0400

    autenticación de google

--- src/lib/views/templatelogin.js
+++ src/lib/views/templatelogin.js
@@ -18,51 +18,46 @@ export const login = () => {
             <button id = "btngoogle" class = "btngoogle"><img src= "imagenes/logogoogle.png" class= "logogoogle">Iniciar Sesión</button>
 
         </div>`
-        divLogin.innerHTML = viewLogin;
+    divLogin.innerHTML = viewLogin;
 
-        const btn = divLogin.querySelector(".btnContinuar")
-        btn.addEventListener("click", async (e) => {
-            e.preventDefault();
-            const emailUser = divLogin.querySelector("#email").value;
-            const pswUser = divLogin.querySelector("#contraseña").value;
-            // const masking = psw.replace(g, '.') ;
-            // userLogin(email, psw);
-            const loginUser = await userLogin(emailUser,pswUser);
+    const btn = divLogin.querySelector(".btnContinuar")
+    btn.addEventListener("click", async(e) => {
+        e.preventDefault();
+        const emailUser = divLogin.querySelector("#email").value;
+        const pswUser = divLogin.querySelector("#contraseña").value;
+        // const masking = psw.replace(g, '.') ;
+        // userLogin(email, psw);
+        const loginUser = await userLogin(emailUser, pswUser);
 
-             if (loginUser){
-             changeRouter('#/wall')
-            }
-            else {
-              console.log("no se ha encontrado Usuario")
-            }
-            //dado que userLogin devuelve una promesa, a esta promesa  aplicarle .then y dentro del then, navegar
-        })
-
-        const btnGoogle = divLogin.querySelector("#btngoogle")
-         btnGoogle.addEventListener("click", async () => {
-          const loginGoogle = await usersGoogle();
-
-          if (loginGoogle){
+        if (loginUser) {
             changeRouter('#/wall')
-           }
-          else {
-             console.log("Problema de conexion con google")
-           }
-           
-       });
-          //const prueba =  getRedirectResult(auth)
-      const redirectGoogle = async() =>{
-      const redirectUser = await getRedirectResult(auth);
-        
-            if (redirectUser){
+        } else {
+            console.log("no se ha encontrado Usuario")
+        }
+        //dado que userLogin devuelve una promesa, a esta promesa  aplicarle .then y dentro del then, navegar
+    })
+
+    const btnGoogle = divLogin.querySelector("#btngoogle")
+    btnGoogle.addEventListener("click", async() => {
+        const loginGoogle = usersGoogle();
+
+        if (loginGoogle) {
             changeRouter('#/wall')
-           }
-       
-      };
-       redirectGoogle();
-          return divLogin;
+        } else {
+            console.log("Problema de conexion con google")
+        }
 
- }; 
- 
+    });
+    //const prueba =  getRedirectResult(auth)
+    const redirectGoogle = async() => {
+        const redirectUser = await getRedirectResult(auth);
 
+        if (redirectUser) {
+            changeRouter('#/wall')
+        }
 
+    };
+    redirectGoogle();
+    return divLogin;
+
+};
