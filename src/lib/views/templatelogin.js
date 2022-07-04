8f8e82d2a7b7be27e356a1d37d9a856214ce3506
keymergamez <keymergamez@gmail.com>
2022-07-04 11:45:26 -0400

    Ultimos arreglos de Autenticacion

--- src/lib/views/templatelogin.js
+++ src/lib/views/templatelogin.js
@@ -1,11 +1,12 @@
-
-import { usersGoogle, userLogin } from "../../firebase/auth.js";
+import { userLogin, userRedirect, usersGoogle } from "../../firebase/auth.js";
+import { auth, getRedirectResult } from "../../firebase/init.js";
+import { changeRouter } from "../router.js";
 
 export const login = () => {
     const divLogin = document.createElement("div")
     const viewLogin =
         `<div class="container-login">
-           
+
             <h1>Gluten Free Food</h1>
             <form class="form">
             <input type="email" id="email" class= "email"  placeholder="E-mail"  required/>
@@ -15,35 +16,53 @@ export const login = () => {
             <button class = "btnregister"><a href='#/register'>Registrar</a></button>
             <button class = "forgot">¿Has olvidado tu contraseña?</button>
             <button id = "btngoogle" class = "btngoogle"><img src= "imagenes/logogoogle.png" class= "logogoogle">Iniciar Sesión</button>
-           
+
         </div>`
         divLogin.innerHTML = viewLogin;
 
         const btn = divLogin.querySelector(".btnContinuar")
-        btn.addEventListener("submit", async (e) => {
+        btn.addEventListener("click", async (e) => {
             e.preventDefault();
-            const email = divLogin.querySelector("#email").value;
-            const psw = divLogin.querySelector("#contraseña").value;
+            const emailUser = divLogin.querySelector("#email").value;
+            const pswUser = divLogin.querySelector("#contraseña").value;
             // const masking = psw.replace(g, '.') ;
-    
-    
-            userLogin(email, psw);
-    
+            // userLogin(email, psw);
+            const loginUser = await userLogin(emailUser,pswUser);
+
+             if (loginUser){
+             changeRouter('#/wall')
+            }
+            else {
+              console.log("no se ha encontrado Usuario")
+            }
+            //dado que userLogin devuelve una promesa, a esta promesa  aplicarle .then y dentro del then, navegar
         })
+
         const btnGoogle = divLogin.querySelector("#btngoogle")
-        btnGoogle.addEventListener("click", () => {
-    
-            console.log(btnGoogle);
-    
-            // const masking = psw.replace(g, '.') ;
-    
-    
-            usersGoogle();
-    
-        })
-    
-    
-    
-        return divLogin;
-    };
-    
+         btnGoogle.addEventListener("click", async () => {
+          const loginGoogle = await usersGoogle();
+
+          if (loginGoogle){
+            changeRouter('#/wall')
+           }
+          else {
+             console.log("Problema de conexion con google")
+           }
+           
+       });
+          //const prueba =  getRedirectResult(auth)
+      const redirectGoogle = async() =>{
+      const redirectUser = await getRedirectResult(auth);
+        
+            if (redirectUser){
+            changeRouter('#/wall')
+           }
+       
+      };
+       redirectGoogle();
+          return divLogin;
+
+ }; 
+ 
+
+
