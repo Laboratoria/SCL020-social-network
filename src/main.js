819653f6f865d9e958847dbb28a3dd53a014a2d3
FernandaFloresC <fernanda.flores.c.99@gmail.com>
2022-07-04 12:17:07 -0400

    error 2Â

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import { indexScreen } from './lib/views/templateIndex';
 import { loginScreen } from './lib/views/templateLogin.js';
 import { registerScreen } from './lib/views/templateRegister.js';
-import { menu } from './lib/views/templateMenu';
+import { menu } from './lib/views/templateMenu.js';
 import { changeroutes } from './lib/routes';
 
 
