6eb0474b29f203f814fa8702520fd28b0d6f66c6
unknown <melhungn@gmail.com>
2022-07-08 13:03:00 -0400

    configuracion firestore, modificacion configuracion firebase, organizacion modulos

--- src/lib/firebase/init.js
+++ src/lib/firebase/init.js
@@ -1,19 +1,23 @@
 //Conecxión con Firebase
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js'
-//Importando funciones firebase
-import { getAuth, signInWithEmailAndPassword,  signInWithPopup, GoogleAuthProvider  } from 'https://www.gstatic.com/firebasejs/9.8.4/firebase-auth.js'
+//Se trae la configuración
 import {firebaseConfig}  from './lib/firebase/config.js'
-import { getFirestore } from "firebase/firestore";
+//Importando funciones firebase auth
+import { getAuth, signInWithEmailAndPassword,  signInWithPopup, GoogleAuthProvider  } from 'https://www.gstatic.com/firebasejs/9.8.4/firebase-auth.js'
+//Importando funciones relacionadas a firestore
+import { getFirestore, collection, getDocs, addDoc,onSnapshot, query, doc } from 'https://www.gstatic.com/firebasejs/9.8.4/firebase-firestore.js';
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 
+// Get firestore
+const db = getFirestore(app);
+
 //Autenticación
 const auth = getAuth();
 
-
 const provider = new GoogleAuthProvider();
 
-//Exportar funciones firebase
-export { initializeApp, signInWithEmailAndPassword, auth, signInWithPopup, provider, GoogleAuthProvider}
+//Exportar funciones firebase-firestore
+export { initializeApp, signInWithEmailAndPassword, auth, signInWithPopup, provider, GoogleAuthProvider, collection, getDocs, addDoc,onSnapshot, query, doc };
 
