a902c08e8b0dbe83684303a54df590b9aa901238
FernandaFloresC <fernanda.flores.c.99@gmail.com>
2022-07-08 13:01:52 -0400

    cambios de css en 1vista

--- src/lib/views/templateMenu.js
+++ src/lib/views/templateMenu.js
@@ -1,23 +1,26 @@
-
+import { login } from "./templateLogin.js";
 //Función que contiene vista del Menu
 export const menu = () => {
-    const menuScreen = `<div>
-      <img src="images/deformitoLlorando.png" width="200px" height="200px">
-    <section class="containerIndex">
+    const menuScreen = `<div><section class="containerIndex">
+    <div class="tips">
+      <img src="images/deformitoTip.png" width="200px" height="200px">
+      Tips Adulto Independiente
+    </div>
+    
       <div class="comunity">
       Únete a la comunidad y 
       conoce todos los secretos
       para vivir sol@ antes de los 30
       (づ￣ 3￣)づ
-        
-    <button class="logIn"> <a href="templateLogin.js">     
+    </div>
+    <button class="logIn"><a href="templateLogin.js">  
      Inicia sesión 
       (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧  
-    </button></div></a>
+    </button></a>
     <section class="signUp"><p>
-      ¿No tienes cuenta aún? Regístrate <a href="">aquí.</a>  
-    </p></section>
-    <img src="images/deformitoTierno.png" width="250px" height="200px" >
+      ¿No tienes cuenta aún? Regístrate <a href="https://img.freepik.com/vector-gratis/pagina-error-404-distorsion_23-2148105404.jpg?w=2000">aquí.</a>  
+    </p>
+    <img src="images/deformitoTierno.png" width="250px" height="200px" ></section>
     </div></section>`;
     return menuScreen;
 };
