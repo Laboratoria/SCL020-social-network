ec640024d302c0437082f7e9354aaae320a46ca2
FernandaFloresC <fernanda.flores.c.99@gmail.com>
2022-07-08 01:13:43 -0400

    vistas e img de prueba

--- src/lib/views/templateMenu.js
+++ src/lib/views/templateMenu.js
@@ -2,21 +2,22 @@
 //Función que contiene vista del Menu
 export const menu = () => {
     const menuScreen = `<div>
-    <ul> 
-    <li>Menú</li>
-    <li>
-      <a href="#/">Home</a>
-    </li>
-    <li>
-      <a href="#/profile">Perfil</a>
-    </li>
-    <li>
-    <a href="#/feed">Muro</a>
-    </li>
-    <li>
-      <a href="#/logOut">Cerrar Sesión</a>
-    </li>
-  </ul>
-    </div>`;
+      <img src="images/deformitoLlorando.png" width="200px" height="200px">
+    <section class="containerIndex">
+      <div class="comunity">
+      Únete a la comunidad y 
+      conoce todos los secretos
+      para vivir sol@ antes de los 30
+      (づ￣ 3￣)づ
+        
+    <button class="logIn"> <a href="templateLogin.js">     
+     Inicia sesión 
+      (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧  
+    </button></div></a>
+    <section class="signUp"><p>
+      ¿No tienes cuenta aún? Regístrate <a href="">aquí.</a>  
+    </p></section>
+    <img src="images/deformitoTierno.png" width="250px" height="200px" >
+    </div></section>`;
     return menuScreen;
 };
