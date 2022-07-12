12e5317dabeaf2409ce2936dacf25c9d75812147
katherine <galleguillos.cantuarias@gmail.com>
2022-07-12 10:31:22 -0400

    boton

--- src/lib/views/templateLogin.js
+++ src/lib/views/templateLogin.js
@@ -1,4 +1,36 @@
-import { changeRoutes } from "../lib/router";
+
+import { async } from "regenerator-runtime";
+import { changeRoutes } from "../lib/router.js";
+/*
+export const login = () => {
+  const screenLogin = document.createElement("div")
+  const viewLogin =
+  ` <div class="container-log">
+  <h1> Adulto Independiente</h1>
+  <button id="btngoogle" id="btngoogle">Inicia Sesión</button>
+  </div>
+  `
+  screenLogin.innerHTML= viewLogin;
+
+  const btnGoogle = screenLogin.querySelector("#btngoogle")
+  btnGoogle.addEventListener("click", async() => {
+    const loginWithGoogle = usersGoogle();
+    if(loginWithGoogle) {
+      changeRoutes('#/feed')
+    } else {
+      changeRoutes('#/error')
+    }
+  });
+
+ return screenLogin;
+
+};*/
+
+
+
+
+
+
 //Funcion que crea el template de la pantalla Login
 export const login = () => {
   const screenLogin = `<section>
