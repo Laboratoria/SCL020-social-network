3ed50409bfba5cbc635dbfed836cd546f033cd9b
unknown <melhungn@gmail.com>
2022-07-12 15:05:40 -0400

    correcciones

--- src/lib/firebase/init.js
+++ src/lib/firebase/init.js
@@ -1,23 +1,56 @@
-//Conexión con Firebase
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js'
-//Se trae la configuración
-import {firebaseConfig}  from './lib/firebase/config.js'
-//Importando funciones firebase auth
-import { getAuth, signInWithEmailAndPassword,  signInWithPopup, GoogleAuthProvider  } from 'https://www.gstatic.com/firebasejs/9.8.4/firebase-auth.js'
-//Importando funciones relacionadas a firestore
-import { getFirestore, collection, getDocs, addDoc,onSnapshot, query, doc } from 'https://www.gstatic.com/firebasejs/9.8.4/firebase-firestore.js';
+// Este es el punto de entrada de tu aplicacion
+//Rutas
+import { home } from './lib/views/templateHome.js';
+import { login } from './lib/views/templateLogin.js';
+import { menu } from './lib/views/templateMenu.js';
+import { changeRoutes } from './lib/router.js';
+import { myFunction} from './lib/index.js';
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+//funciones firestore
+import { savePost } from './lib/firebase/firestore.js';
 
-// Get firestore
-const db = getFirestore(app);
 
-//Autenticación
-const auth = getAuth();
 
-const provider = new GoogleAuthProvider();
+//Contiene o llama el div del html
+export const root =  document.getElementById('root');
 
-//Exportar funciones firebase-firestore
-export { initializeApp, signInWithEmailAndPassword, auth, signInWithPopup, provider, GoogleAuthProvider, collection, getDocs, addDoc,onSnapshot, query, doc };
+//Init del router
+const init = () => {
+    document.getElementById('root').innerHTML = menu();
+    window.addEventListener('hashchange', () => {
+        myFunction();
+        console.log("holaa");
+        changeRoutes(window.location.hash);
+    })
+}
+//carga el init
+window.addEventListener('load', init);
 
+
+
+
+//llama al input y textarea
+export const title = document.getElementById('postTitle');
+export const postText = postform['postText'];
+
+//Formulario enviado DOM
+export const postForm = document.getElementById('postform');
+postForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    title;
+    postText;
+
+    // const title = document.getElementById('postTitle');
+    // const postText = postform['postText'];
+
+//console.log(title.value);
+
+   //obtiene el valor del titulo y el post del input y textarea
+ console.log (savePost(title.value, postText.value));
+   // console.log(postText.value)
+
+   //Resetea el formulario, lo limpia
+   postForm.reset();
+
+});
--- src/lib/firebase/templatePost.js
+++ src/lib/firebase/templatePost.js
@@ -1,11 +1,10 @@
 //import { docData } from "./firestore"
 
-
 //Template del post
 export const post = (data) => {
     const viewPost = `<div> 
     <h3>${data.title}</h3>
-    <p>${data.postText}</p>
-    </div>`
+    <p>${data.text}</p>
+    </div>`;
  return viewPost;
 } 
