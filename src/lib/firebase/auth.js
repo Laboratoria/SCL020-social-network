08a19e290126a7f83fcc1fa906a76a6159033058
unknown <melhungn@gmail.com>
2022-07-15 12:34:44 -0400

    test

--- src/lib/firebase/auth.js
+++ src/lib/firebase/auth.js
@@ -38,3 +38,5 @@ signInWithPopup(auth, provider)
     // ...
   });
 }
+
+
--- src/lib/firebase/init.js
+++ src/lib/firebase/init.js
@@ -4,7 +4,7 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.8.4/firebase
 //configuracion firebase
 import { firebaseConfig } from './config.js';
 //Importando funciones firebase auth
-import { getAuth, signInWithEmailAndPassword,  signInWithPopup, GoogleAuthProvider  } from 'https://www.gstatic.com/firebasejs/9.8.4/firebase-auth.js';
+import { getAuth, signInWithEmailAndPassword,  signInWithPopup, GoogleAuthProvider, onAuthStateChanged  } from 'https://www.gstatic.com/firebasejs/9.8.4/firebase-auth.js';
 //Importando funciones relacionadas a firestore
 import { getFirestore, collection, getDocs, addDoc,onSnapshot, query, doc, getDoc, deleteDoc } from 'https://www.gstatic.com/firebasejs/9.8.4/firebase-firestore.js';
 
@@ -12,15 +12,18 @@ import { getFirestore, collection, getDocs, addDoc,onSnapshot, query, doc, getDo
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 //Autenticación
 const auth = getAuth();
+
 //Provider
 const provider = new GoogleAuthProvider();
+
 // Get firestore
 const db = getFirestore(app);
 
 
 
 //Exportar funciones firebase-firestore
-export { initializeApp, signInWithEmailAndPassword, auth, signInWithPopup, provider, GoogleAuthProvider, collection, getDocs, addDoc,onSnapshot, query, doc, db, deleteDoc, getDoc };
+export { initializeApp, signInWithEmailAndPassword, auth, signInWithPopup, provider, GoogleAuthProvider, collection, getDocs, addDoc,onSnapshot, query, doc, db, deleteDoc, getDoc, onAuthStateChanged };
 
